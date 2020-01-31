da3deed512a382d9e45aaf14b349dd6ff217de29
Ben <ben.peltier7@gmail.com>
2020-01-31 18:33:23 +0100

    Basic movement

--- BrawlerProject/Assets/Scripts/MovementController.cs
+++ BrawlerProject/Assets/Scripts/MovementController.cs
@@ -6,9 +6,16 @@ public class MovementController : MonoBehaviour
     public static MovementController instance;
     public Vector2 movementDirection;
     public float movementSpeed;
+    public float rotationSpeed = 1;
 
-    Rigidbody rigid;
+    CharacterController controller;
+    Animator animController;
     InputMap inputMap;
+    Transform rotationTarget;
+    Camera cam;
+
+    Vector3 forward;
+    Vector3 right;
     private void OnEnable() => inputMap.InGame.Enable();
     private void OnDisable() => inputMap.InGame.Disable();
 
@@ -24,11 +31,42 @@ public class MovementController : MonoBehaviour
         inputMap.InGame.Movement.performed += ctx => movementDirection = ctx.ReadValue<Vector2>();
         inputMap.InGame.Movement.canceled += ctx => movementDirection = Vector2.zero;
 
-        rigid = GetComponent<Rigidbody>();
+        animController = GetComponent<Animator>();
+        rotationTarget = new GameObject("RotationTargetObject").transform;
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
 
     private void Update()
     {
-        rigid.AddForce(new Vector3(movementDirection.x,0,movementDirection.y) * movementSpeed * Time.deltaTime);
+        AnimationHandler();
+        PlayerMovement();
+        PlayerRotation();
+    }
+
+    void PlayerRotation()
+    {
+        if(movementDirection != Vector2.zero)
+        {
+            rotationTarget.position = transform.position + (movementDirection.x * right + forward * movementDirection.y);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rotationTarget.position - transform.position), Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    void PlayerMovement()
+    {
+        forward = cam.transform.forward;
+        right = cam.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        Vector3 movement = (movementDirection.x * right + forward * movementDirection.y) * (movementSpeed * movementDirection.magnitude) * Time.deltaTime;
+        controller.Move(movement);
+    }
+
+    void AnimationHandler()
+    {
+        animController.SetFloat("SpeedMagnitude", movementDirection.magnitude);
     }
 }
